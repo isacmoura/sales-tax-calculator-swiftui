b369740526c62357c77a0ae2ee6bf2141aae8ce1
Isac Moura <contatoisacmoura@outlook.com>
2022-11-07 08:47:47 -0300

    Creating Sales Tax Calculator using SwiftUI

---
+++ SalesTax-SwiftUI/SalesTax_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesTax_SwiftUIApp.swift
+//  SalesTax-SwiftUI
+//
+//  Created by Isac Moura Gomes on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SalesTax_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
